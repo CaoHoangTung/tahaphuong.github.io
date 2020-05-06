1b3c40e0bf8b3511cba3b269e9277d550ab232a9
tahaphuong <phuonghata1995@gmail.com>
2020-05-06 07:57:18 +0700

    grammar fix

---
+++ de.js
@@ -0,0 +1,440 @@
+
+  const workHTML = {
+
+    "artwork-title": `
+    <div class="skip work-showcase-artworks" id="work-showcase-artworks">
+      <div class="skip work-showcase-artworks-pallete">
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('f0e2d3', this)" style="background-color: #f0e2d3"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('f0e2d3', this)">f0e2d3</div>
+        </div>
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('f3ceb4', this)" style="background-color: #f3ceb4"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('f3ceb4', this)">f3ceb4</div>
+        </div>
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('ffdecf', this)" style="background-color: #ffdecf"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('ffdecf', this)">ffdecf</div>
+        </div>
+
+      </div>
+    <img src="./img/artworks.jpg"/>
+
+
+      <div class="skip work-showcase-artworks-pallete">
+
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('e4d9b4', this)" style="background-color: #e4d9b4"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('e4d9b4', this)">e4d9b4</div>
+        </div>
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('c9b998', this)" style="background-color: #c9b998"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('c9b998', this)">c9b998</div>
+        </div>
+
+        <div class="skip work-showcase-artworks-color">
+          <div class="skip work-showcase-artworks-copied">kopiert</div>
+          <div class="skip work-showcase-artworks-color-ball" onclick="copyOnClick('8f9072', this)" style="background-color: #8f9072"></div>
+          <div class="skip work-showcase-artworks-color-name" onclick="copyOnClick('8f9072', this)">8f9072</div>
+        </div>
+      </div>
+    </div>
+    `,
+
+    "webapp-title": `
+    <div class="skip work-showcase-webapp">
+      <div class="skip">
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/borrowmoney.png');"></div>
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">User can use this web app to manage the money log, when they lend/borrow money.</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://borrowmoney-thp.herokuapp.com/">Demo link</a>
+            <a target="_blank" href="https://github.com/tahaphuong/borrowMoney">Github link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="skip">
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">A simple chat webapp.</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://tahaphuong.github.io/chat">Demo link</a>
+            <a target="_blank" href="https://github.com/tahaphuong/chat">Github link</a>
+          </div>
+        </div>
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/chat.png');"></div>
+      </div>
+
+      <div class="skip">
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/eclass.png');"></div>
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">A homework platform (with deadline) connecting teachers and students.</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://eclass-thp.herokuapp.com/">Demo link</a>
+            <a target="_blank" href="https://github.com/tahaphuong/eclass">Github link</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    `,
+
+    "staticweb-title": `
+
+    <div class="skip work-showcase-webapp">
+      <div class="skip">
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/health.png');"></div>
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">My first website! You can take a health&lifestyle test and find some tips here. (self-design)</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://tahaphuong.github.io/health">Demo link</a>
+            <a target="_blank" href="https://github.com/tahaphuong/health">Github link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="skip">
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">A landing page for my school's art club. (self-design)</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://hsgsartclub.github.io">Demo link</a>
+            <a target="_blank" href="https://github.com/hsgsartclub/hsgsartclub.github.io">Github link</a>
+          </div>
+        </div>
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/hac.png');"></div>
+      </div>
+
+      <div class="skip">
+        <div class="skip work-showcase-webapp-img" style="background-image: url('./img/udic.png');"></div>
+        <div class="skip work-showcase-webapp-block">
+          <div class="skip work-showcase-webapp-des">A landing page I created, based on available design.</div>
+          <div class="skip work-showcase-webapp-link">
+            <a target="_blank" href="https://tahaphuong.github.io/udicTest">Demo link</a>
+            <a target="_blank" href="https://github.com/tahaphuong/udicTest">Github link</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    `,
+
+    "reactnative-title": `
+    <div class="skip work-showcase-reactnative" id="work-showcase-reactnative">
+          <img src="./img/film.jpg"/>
+          <div class="skip work-showcase-reactnative-block">
+            <div class="skip work-showcase-reactnative-block-title">A film app</div>
+            <br><br>
+            <div class="skip work-showcase-reactnative-block-des">
+              When user creates an acount, a user store will be automatically created on firestore. User can look up film and save the film to the download list.
+              <br><br>
+              I use "global state" to make sure the data changes will be updated in all screens.
+              <br><br>
+              I also applied "Lazy load" with Flatlist in order to enhance the performance on the "Search" screen.
+              <br><br>
+              It took me around 1 week to get used to React Native and create the UI for this app, and around 2 weeks to finish the rest.
+              <br><br><br>
+              <a target="_blank" href="https://github.com/tahaphuong/filmRN">Github link</a>
+            </div>
+          </div>
+        </div>
+    `
+  }
+
+  
+  // WINDOW ONLOAD
+  let currentWork = 'artwork-title';
+  let prevScroll = 0;
+  let prevBall = "home";
+
+  window.onload = init;
+
+  function init() {
+    let main = document.getElementById("main");
+
+    let loadScreen = document.getElementById("load") 
+    loadScreen.style.transition = "all 0.4s";
+    loadScreen.style.opacity = "0";
+
+    setTimeout(function(){
+      loadScreen.style.display = "none";
+      main.style.opacity = "1"
+    }, 400)
+
+    scrollToTop();
+    // handle scroll event
+    $(window).scroll(function () {
+      let scrollingPos = window.pageYOffset;
+        // when scroll to bottom
+        if (scrollingPos>prevScroll) {
+          divAppear(scrollingPos, "quote"); 
+          divAppear(scrollingPos, "work"); 
+          divAppear(scrollingPos, "about");
+          divAppear(scrollingPos, "contact");
+        } 
+
+        // when scroll to top
+        if (scrollingPos <= prevScroll) {
+          divDisappear(scrollingPos, "quote"); 
+          divDisappear(scrollingPos, "work"); 
+          divDisappear(scrollingPos, "about");
+          divDisappear(scrollingPos, "contact");
+        } 
+
+        // get prevScroll => compare to scroll position 
+        //=> in order to check scroll direction
+
+        prevScroll = scrollingPos;
+    });
+    
+    cursorBall("soundcloud-link", "soundcloud-ball");
+    cursorBall("hec-link", "hec-ball");
+  }
+
+
+
+    // make div appear on scroll to bottom
+  function divAppear(scrollingPos, id) {
+    let section = document.getElementById(id)
+
+
+    if (section.offsetTop - scrollingPos < window.innerHeight) {
+      switch (id) {
+        case 'quote': 
+          chooseNav('home');
+          document.getElementById("quote-doublequotes").style.animation = "doublequotes 10s cubic-bezier(.3,.6,.7,.9) infinite"
+          document.getElementById("quote-hand-img").style.animation = "handOut 5s cubic-bezier(.3,.6,.7,.9) infinite";
+        break;
+        case 'work': 
+          chooseNav('work');
+        break;
+        case 'about': 
+          chooseNav('about');
+        break;
+        case 'contact': 
+          chooseNav('contact');
+          document.getElementById("link-cv").innerHTML = 'see my CV (pdf)';
+        break;
+        default: 
+          console.log('scroll error')
+        break;
+      }
+
+      let list = section.getElementsByTagName("div")
+      for (let ele of list) {
+        if (ele.offsetTop - scrollingPos <= window.innerHeight*0.8 && !ele.classList.contains("skip")) { // can not scroll further than bottom of page
+          ele.style.animation = "appear 0.7s cubic-bezier(0.25,0.75,0.5,1) forwards"
+        }
+      }
+
+    }
+  }
+  
+  // make div disappear on scroll to top
+
+  function divDisappear(scrollingPos, id) {
+
+    let section = document.getElementById(id);
+
+    if (section.offsetTop - scrollingPos > window.innerHeight && section.offsetTop - scrollingPos < window.innerHeight + section.offsetHeight) {
+
+      switch (id) {
+        case 'quote': 
+          chooseNav('home');
+        break;
+        case 'work': 
+          chooseNav('home');
+        break;
+        case 'about': 
+          chooseNav('work');
+        break;
+        case 'contact': 
+          chooseNav('about');
+          document.getElementById("link-cv").innerHTML = 'my CV';
+        break;
+
+        default: 
+          console.log('scroll error')
+        break;
+      }
+      let list = section.getElementsByTagName("div")
+      for (let ele of list) {      
+        // the elements in "skip" class will not be affected.
+        if (!ele.classList.contains("skip")) {
+          ele.style.animation = "disappear 0.7s ease-out forwards"
+        }
+      }
+
+    }
+  }
+
+  function chooseNav(nav) {
+    document.getElementById("nav-ball-" + prevBall).style.opacity = "0";
+    document.getElementById("nav-ball-" + nav).style.opacity = "1";
+  
+    prevBall = nav;
+  }
+
+  function workBlockOnhover(id) {
+    if (id != currentWork) {
+      document.getElementById(id).style.backgroundPosition = '-100%';
+    }
+   }
+
+  function workBlockOuthover(id) {
+    if (id != currentWork) {
+      document.getElementById(id).style.backgroundPosition = '0';
+    }
+  }
+
+  function workBlockOnclick(id) {
+    document.getElementById(currentWork).style.backgroundPosition = '0';
+    document.getElementById(id).style.backgroundPosition = '-100%';
+
+    currentWork = id;
+
+    let workMain = document.getElementById("work-main")
+    $("html, body").stop().animate({scrollTop: workMain.offsetTop}, 700, 'swing');
+
+    let showcase = document.getElementById("work-showcase");
+
+    showcase.innerHTML = workHTML[currentWork]
+
+  }
+
+
+function copyOnClick(content, com) {  
+  // create textarea to copy
+  let textArea = document.createElement('textarea');
+  textArea.opacity = "0";
+  textArea.value = content;
+  document.body.append(textArea)
+
+  textArea.select();
+  textArea.setSelectionRange(0, 100);
+  document.execCommand("copy");
+
+  document.body.removeChild(textArea)
+
+  // alert
+  let copied = com.parentNode.children[0];
+
+  copied.style.opacity = "1";
+  setTimeout(function(){copied.style.opacity = "0"}, 800)
+}
+
+
+// function cursorBall 
+
+function cursorBall(secId, ballId) {
+  let section = document.getElementById(secId)
+  let ballCursor = document.getElementById(ballId)
+
+  section.onmousemove = function(e) {
+    if (e.pageY < section.offsetTop + section.offsetHeight + 50 && e.pageY > section.offsetTop - 50) {
+      ballCursor.style.display = 'flex';
+      ballCursor.style.top = String(e.pageY) + 'px';
+      ballCursor.style.left = String(e.pageX) + 'px';
+    }
+  }
+
+  section.onmouseout = function(e) {
+    ballCursor.style.display = 'none';
+  }
+}
+
+
+function scrollToTop() {
+  $("html, body").stop().animate({scrollTop: 0}, 700, 'swing');
+}
+
+function navScroll(id) {
+  let sectionHeight = document.getElementById(id).offsetTop;
+  console.log(sectionHeight)
+
+  $("html, body").stop().animate({scrollTop: sectionHeight}, 700, 'swing');
+
+  if (window.innerWidth < 812 || window.innerWidth/window.innerHeight < 1.5 ) {
+    setTimeout(()=>{toggleNavMobile()}, 500)
+
+  }
+}
+
+function textAppearsOnHover(textId) {
+  document.getElementById(textId).style.opacity = "1";
+}
+
+function textDisappearsOutHover(textId) {
+  document.getElementById(textId).style.opacity = "0";
+}
+
+// toggle nav mobile 
+
+function toggleNavMobile() {
+  let nav = document.getElementById("nav-mobile");
+  let divs = document.getElementById("nav-mobile").getElementsByTagName('div');
+
+
+  switch (nav.style.display) {
+    case "none":
+      nav.style.display = "flex"
+
+      setTimeout(()=>{
+        nav.style.height = "100vh"
+        for (let el of divs) {
+          el.style.opacity = "1"
+        }
+      }, 50)
+
+    break;
+    default:
+      nav.style.height = "0vh"
+      for (let el of divs) {
+        el.style.opacity = "0"
+      }
+
+      setTimeout(()=>{
+        nav.style.display = "none"
+      }, 400)
+    break;
+  }
+}
+
+// index of the drop
+let rainIndex = 1;
+
+function falling() {
+
+  if (rainIndex<=2000) {
+    let left = Math.floor(Math.random() * Math.floor(100)) + 1; // choose between 1 and 100 vw 
+    let color = Math.floor(Math.random() * Math.floor(4)) + 1 // choose between 1, 2, 3 or 4;
+
+    // add after a random elements
+    $('#rain').after('<div id="rain-' + String(rainIndex) + '" class="deco deco-' + String(color) + '" style="left:' + String(left) + 'vw; z-index: 60"></div>')
+
+    let thisIndex = "rain-" + rainIndex;
+
+    setTimeout(function(){
+      document.getElementById(thisIndex).remove();
+    }, 3000)
+
+    rainIndex += 1;
+  }
+
+}
+
+let doublequotesColor = 1
+function changeColorOnClick(id, mainClasses) {
+  document.getElementById(id).className = mainClasses + " color-" + String(doublequotesColor%4 + 1)
+  doublequotesColor += 1;
+}
+
